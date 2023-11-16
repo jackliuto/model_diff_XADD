5063dba182fefd3bc7fb584724e7b39aaabb8b6c
jackliu <jackliu@pop-os.localdomain>
2023-11-15 22:43:40 -0500

    added policy extraction

---
+++ extra_policy.py
@@ -0,0 +1,117 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn import tree
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+from pyRDDLGym import RDDLEnv
+from policy_learning.envs.navEnv import envWrapperNav
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3 import DQN
+
+# globalvars
+DOMAIN_PATH = "./RDDL/navigation/navigation_disc/domain.rddl"
+INSTANCE_PATH = "./RDDL/navigation/navigation_disc/instance_1agent_source.rddl"
+
+
+
+RDDLEnv = RDDLEnv.RDDLEnv(domain=DOMAIN_PATH, instance=INSTANCE_PATH)
+env = envWrapperNav(RDDLEnv, max_episode_length=1000)
+
+
+loaded_model = DQN.load('./policy_learning/checkpoints/rl_model_9000000_steps.zip', env=env, exploration_fraction=0.0)
+
+obs, _ = env.reset()
+
+q_values = loaded_model.predict(obs)
+
+x = np.arange(0, 11, 1)
+y = np.arange(0, 11, 1)
+
+X, Y = np.meshgrid(x, y)
+Z = np.zeros_like(X, dtype=float)
+
+
+action_list = env.action_list
+observation_list = env.observation_list
+
+inputs = []
+labels = []
+
+for i in range(len(x)):
+    for j in range(len(y)):
+        obs = {'pos_x___a1': np.array([i], dtype=np.float32), 'pos_y___a1': np.array([j], dtype=np.float32)}
+        value, _ = loaded_model.predict(obs)
+        Z[i][j] = value
+        inputs.append(np.array([i,j]))
+        labels.append(env.action_list[value])
+
+
+inputs = np.array(inputs)
+labels = np.array(labels)
+
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(inputs, labels)
+
+fig = plt.figure(figsize=(50, 30))
+tree.plot_tree(clf)
+fig.savefig('test_tree.png')
+
+print(Z.T)
+
+y_pred = clf.predict(inputs)
+
+Z_tree = np.empty(X.shape, dtype=float)
+
+for idx in range(len(inputs)):
+    i = inputs[idx][0]
+    j = inputs[idx][1]
+    value = y_pred[idx]
+    Z_tree[i][j] = action_list.index(value)
+
+
+def export_dict(clf, feature_names=None):
+    tree = clf.tree_
+    if feature_names is None:
+        feature_names = range(clf.max_features_)
+    
+    # Build tree nodes
+    tree_nodes = []
+    for i in range(tree.node_count):
+        if (tree.children_left[i] == tree.children_right[i]):
+            tree_nodes.append(
+                clf.classes_[np.argmax(tree.value[i])]
+            )
+        else:
+            tree_nodes.append({
+                "feature": feature_names[tree.feature[i]],
+                "value": tree.threshold[i],
+                "left": tree.children_left[i],
+                "right": tree.children_right[i],
+            })
+    
+    # Link tree nodes
+    for node in tree_nodes:
+        if isinstance(node, dict):
+            node["left"] = tree_nodes[node["left"]]
+        if isinstance(node, dict):
+            node["right"] = tree_nodes[node["right"]]
+    
+    # Return root node
+    return tree_nodes[0]
+
+print(clf.tree_.feature)
+
+print(export_dict(clf, observation_list))
+
+
+
+
+# # tree_dict = tree_to_dict(clf, tree_rules)
+# tree_dict = tree_text_to_dict(tree_rules)
+
+# print(tree_rules)
+
--- stablebaseline_dqn.py
+++ stablebaseline_dqn.py
@@ -1,37 +1,64 @@
 
 
+import warnings
+warnings.filterwarnings("ignore")
+
 from pyRDDLGym import RDDLEnv
 
 from policy_learning.envs.navEnv import envWrapperNav
 
 from stable_baselines3 import DQN
+from stable_baselines3.common.callbacks import CallbackList, CheckpointCallback, EvalCallback
+from stable_baselines3.common.logger import configure
 
+from stable_baselines3.common.env_checker import check_env
+
+# globalvars
 DOMAIN_PATH = "./RDDL/navigation/navigation_disc/domain.rddl"
-INSTANCE_PATH = "./RDDL/navigation/navigation_disc/instance_1agent_target.rddl"
+INSTANCE_PATH = "./RDDL/navigation/navigation_disc/instance_1agent_source.rddl"
 
+GAMMA = 0.9
+TAU = 0.001
+EXP_BEG = 0.9
+EXP_FINAL = 0.1
+BUFFER_SIZE = 10000
+BATCH_SIZE = 128
+
+total_timesteps = 10000000    # Adjust as needed
+
+
+# set enviroment
 RDDLEnv = RDDLEnv.RDDLEnv(domain=DOMAIN_PATH, instance=INSTANCE_PATH)
-
-env = envWrapperNav(RDDLEnv)
-env.max_episode_length = 1000
-
-model = DQN('MultiInputPolicy', env, verbose=0)
+env = envWrapperNav(RDDLEnv, max_episode_length=1000)
 
 
-total_timesteps = 10000    # Adjust as needed
+check_env(env)
 
-model.learn(total_timesteps=total_timesteps)
+# set callbacks
+checkpoint_callback = CheckpointCallback(save_freq = 1000, 
+                                         save_path = './policy_learning/checkpoints/',
+                                         name_prefix = 'rl_model')
 
-# Access information from the model's info attribute
-train_info = model.info
+# eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/',
+#                              log_path='./logs/', eval_freq=500,
+#                              deterministic=True, render=False)
 
-# Access specific information
-total_timesteps = train_info['total_timesteps']
-time_elapsed = train_info['time_elapsed']
-num_episodes = train_info['num_episodes']
-mean_reward = train_info['ep_rew_mean']
+# set up logger
+tmp_path = "./policy_learning/logs/"
+new_logger = configure(tmp_path, ["stdout", "csv", "log", "tensorboard"])                    
 
-# Print or use the information as needed
-print(f"Total Timesteps: {total_timesteps}")
-print(f"Time Elapsed: {time_elapsed} seconds")
-print(f"Number of Episodes: {num_episodes}")
-print(f"Mean Reward: {mean_reward}")
+model = DQN('MultiInputPolicy', env, 
+            gamma=GAMMA,
+            tau=TAU,
+            exploration_initial_eps=EXP_BEG,
+            exploration_final_eps=EXP_FINAL,
+            buffer_size=BUFFER_SIZE,
+            batch_size=BATCH_SIZE,
+            verbose=1)
+
+model.set_logger(new_logger)
+
+
+training_info = model.learn(total_timesteps=total_timesteps,
+                            callback=checkpoint_callback)
+
